c58abc1279a57029b5be95eb12e554b29d7604bf
lihn1987 <lihn1011@163.com>
2020-04-17 20:58:05 +0800

    0.3.7

--- src/components/h5/header/index.js
+++ src/components/h5/header/index.js
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 import connectWithRouter from 'utils/connectWithRouter';
 import MenuIcon from './MenuIcon';
 import style from './style.scss';
-const openMenu = 'https://cdn.jsdelivr.net/gh/netcloth/official-website@v0.3.6/src/assets/menu.png';
+const openMenu = 'https://cdn.jsdelivr.net/gh/netcloth/official-website@v0.3.7/src/assets/menu.png';
 
 
 const iconConfig = {
